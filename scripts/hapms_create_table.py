38ea45c149ae4006b714a1127b3f0bd04180a2cf
Andyjd86 <dixon.services@outlook.com>
2018-01-24 18:59:57 +0000

    Create HAPMS table import

---
+++ scripts/hapms_create_table.py
@@ -0,0 +1,22 @@
+import psycopg2
+
+conn = None
+try:
+    # Password is contained in a file so that it isn't exposed here.
+    conn = psycopg2.connect("dbname = 'ins_data_dev' user = 'andydixon' host = 'localhost'")
+except:
+    print("I am unable to connect the database")
+cur = conn.cursor()
+fpath = 'C:\\Users\\adixon\\Desktop\\Projects\\INS Database\\ins-database\\data\\text\\TRL - Section data_M6_M27_A47.csv'
+# Table name to use in function
+args = ["letstryagain"]
+tblname = 'letstryagain'
+# Calls the function in PostgreSQL
+cur.callproc("client.create_table_hapms", args)
+conn.commit()
+SQLstate = "COPY client.%s FROM STDIN WITH CSV HEADER DELIMITER AS ',';" % tblname
+print(SQLstate)
+cur.copy_expert(SQLstate, open(fpath))
+conn.commit()
+cur.close()
+conn.close()
--- scripts/import_shape.py
+++ scripts/import_shape.py
@@ -1,14 +1,17 @@
-import os, subprocess
+import os
+import subprocess
 
 # Choose your PostgreSQL version here
 os.environ['PATH'] += r';C:\Program Files\PostgreSQL\9.6\bin'
 # http://www.postgresql.org/docs/current/static/libpq-envars.html
+# Password is contained in a file so that it isn't exposed here.
 os.environ['PGHOST'] = 'localhost'
 os.environ['PGPORT'] = '5432'
 os.environ['PGUSER'] = 'andydixon'
 os.environ['PGPASSFILE'] = r'C:\Users\adixon\AppData\Roaming\postgresql\pgpass.conf'
 os.environ['PGDATABASE'] = 'ins_data_dev'
 
+# Choose base directory to search in, anticipated that this will be fixed and managed by admin.
 base_dir = r"C:\Users\adixon\Desktop\Projects\INS Database\ins-database\data\shp"
 srid = '27700'
 shp_import = 'hapms_temp'
@@ -19,6 +22,10 @@ for source, dirs, files in full_dir:
         if file_[-3:] == 'shp':
             shapefile_path = os.path.join(base_dir, file_)
             shapefile_list.append(shapefile_path)
+# Future development adds all the files to a form to allow tick box selection and addition of table name and SRID values
+# to populate the variables above.
+# Calls the command line function shp2pgsql with several arguments including one to mute the output. Future development
+# should put this output to a log file for error catching.
 for shape_path in shapefile_list:
     cmds = 'shp2pgsql -d -I -s "' + srid + '" "' + shape_path + '" "' + shp_import + '" | psql '
-    subprocess.call(cmds, shell=True)
+    subprocess.run(cmds, shell=True, stdout=subprocess.DEVNULL)
